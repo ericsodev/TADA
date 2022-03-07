ec514674d919c5b1e55f7223dcacf757bcfcca76
Eric <ericsodev@gmail.com>
2022-03-06 21:28:08 -0500

    Removed unnecessary Mongoose model constraints

--- server/src/models/Todo.ts
+++ server/src/models/Todo.ts
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 import { Schema, model } from "mongoose";
 
 const TodoSchema = new Schema({
-    _id: { type: mongoose.Schema.Types.ObjectId, index: true, required: true, auto: true },
-    name: { type: String, required: true },
-    createdDate: { type: Date, required: true },
-    dueDate: Date,
-    completed: { type: Boolean, required: true },
-    priority: { type: String, required: true },
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+  name: { type: String, required: true },
+  createdDate: { type: Date, required: true },
+  dueDate: Date,
+  completed: { type: Boolean, required: true },
+  priority: { type: String, required: true },
 });
 
 export default model("Todo", TodoSchema);
